54c830d70cd1840e6c9a8916c6903da825711a75
Nawin Chander S <56625450+Nawinchander@users.noreply.github.com>
2024-03-09 23:59:54 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,23 @@
+// import { useState } from 'react'
+import Advice_App from "./Components/Advice_App"
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div>
+
+      <Advice_App />
+
+    </div>
+
+      
+    </>
+  )
+}
+
+export default App
